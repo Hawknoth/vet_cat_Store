46ffa2673101e1c4076e43628bbb84dd3c02b9aa
Hawknoth <adrian.landeros7309@alumnos.udg.mx>
2025-07-15 21:47:34 -0600

    Update server.js

--- vet-cat-store/server.js
+++ vet-cat-store/server.js
@@ -6,40 +6,34 @@ const db = require('./db');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Motor de vistas y carpeta estática
+// Vistas y estáticos
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta principal con buscador de productos
+// Home (lista + filtros)
 app.get('/', (req, res) => {
   db.all("SELECT * FROM products", (err, products) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send("Error de base de datos");
-    }
+    if (err) return res.status(500).send("Error de base de datos");
     res.render('index', { products });
   });
 });
 
-// Detalle de producto
+// Detalle
 app.get('/product/:id', (req, res) => {
   db.get("SELECT * FROM products WHERE id = ?", [req.params.id], (err, product) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send("Error de base de datos");
-    }
+    if (err) return res.status(500).send("Error de base de datos");
     if (!product) return res.status(404).send("Producto no encontrado");
     res.render('product', { product });
   });
 });
 
-// Página de carrito (simulada)
+// Carrito simulado
 app.get('/cart', (req, res) => {
   res.render('cart');
 });
 
-// Error handler genérico
+// Error genérico
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('¡Vaya! Algo salió mal.');
@@ -48,3 +42,4 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
+
